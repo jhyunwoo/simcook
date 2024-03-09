5d45db96cad430d7204833a2c0752544fd9070b1
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2024-03-10 08:25:14 +0900

    recipe ui edit(except image)

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -63,6 +63,7 @@ export default function Profile(): ReactElement {
       </fieldset>
 
       {/* 질병 선택 */}
+<<<<<<< Updated upstream
       <fieldset>
         <legend>질병</legend>
         <ul>
@@ -80,6 +81,47 @@ export default function Profile(): ReactElement {
           ))}
         </ul>
       </fieldset>
+=======
+      <div className={"p-2 bg-white rounded-xl flex justify-between"}>
+        <div className={"text-lg font-semibold"}>당뇨</div>
+        <label className="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            checked={isDiabetes}
+            onChange={toggleDiabetes}
+          />
+          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-rose-300 dark:peer-focus:ring-rose-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-rose-600"></div>
+        </label>
+      </div>
+      <div className={"p-2 bg-white rounded-xl flex justify-between"}>
+        <div className={"text-lg font-semibold"}>고혈압</div>
+        <label className="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            checked={isHBP}
+            onChange={toggleHBP}
+          />
+          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-rose-300 dark:peer-focus:ring-rose-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-rose-600"></div>
+        </label>
+      </div>
+      <div className={"p-2 bg-white rounded-xl flex justify-between"}>
+        <div className={"text-lg font-semibold"}>고지혈증</div>
+        <label className="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            checked={isHyperlipidemia}
+            onChange={toggleHyperlipidemia}
+          />
+          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-rose-300 dark:peer-focus:ring-rose-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-rose-600"></div>
+        </label>
+      </div>
+      <div className="pb-8 bg-gray-200 rounded-xl flex justify-between">
+        하루 영양소
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ app/recipe/[cookId]/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { ReactElement, useEffect, useState } from "react";
+import { pb } from "@/lib/pocketbase";
+import Link from "next/link";
+import Image from "next/image";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/24/outline";
+
+export default function Recipe({
+  params,
+}: {
+  params: { cookId: string };
+}): ReactElement {
+  const [recipe, setRecipe] = useState<any>();
+  const [option, setOption] = useState<"basic" | "custom">("basic");
+
+  function handleOption() {
+    setOption(option === "basic" ? "custom" : "basic");
+  }
+
+  useEffect(() => {
+    async function getCookRecipe() {
+      const data = await pb.collection("cook").getOne(params.cookId);
+      setRecipe(data);
+    }
+    getCookRecipe();
+  }, [params.cookId]);
+
+  return (
+    <div className={"w-full min-h-screen flex flex-col p-4 space-y-2"}>
+      <Link href={"/"} className={"flex space-x-2 items-center group py-2"}>
+        <ChevronDoubleLeftIcon className={"size-6"} />
+        <div className={"group-hover:underline"}>홈</div>
+      </Link>
+      <div className={"text-2xl font-bold py-4"}>{recipe?.foodName}</div>
+      <div className={"flex w-full justify-end space-x-2 py-4"}>
+        <button
+          type={"button"}
+          onClick={handleOption}
+          className={`${option === "basic" ? "bg-rose-500 text-white" : ""} p-1 px-2 rounded-lg`}
+        >
+          기본 설정
+        </button>
+        <button
+          type={"button"}
+          onClick={handleOption}
+          className={`${option === "custom" ? "bg-rose-500 text-white" : ""} p-1 px-2 rounded-lg`}
+        >
+          커스텀
+        </button>
+      </div>
+      <div className={"flex flex-row  p-2 bg-white rounded-xl"}>
+        <div className="basis-1/3">난이도: {recipe?.difficulty}/5</div>
+        <div className="basis-1/3">조리 시간: {recipe?.time}</div>
+        <div className="basis-1/3">제작자: {recipe?.writer}</div>
+      </div>
+      <div className="py-4 p-2 bg-white rounded-xl">
+        요리 도구:
+        {recipe?.tools?.map((data: any, index: number) => (
+          <div key={index} className={"flex space-x-2"}>
+            <div>{data?.ToolName}</div>
+          </div>
+        ))}
+      </div>
+      <div className="py-4 p-2 bg-white rounded-xl">
+        요리 재료:
+        {recipe?.ingredient?.map((data: any, index: number) => (
+          <div key={index} className={"flex space-x-2"}>
+            <div>{data?.IngredientName}</div>
+            <div>{data?.amount}</div>
+          </div>
+        ))}
+      </div>
+      <div className="py-4 p-2 bg-white rounded-xl">
+        레시피:
+        {recipe?.recipe?.map((data: any, index: number) => (
+          <div key={index}>{data?.text}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
