9b29934b8c712c5b8b5024197b23b0efc1353bba
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2024-03-10 06:20:51 +0900

    recipe 업데이트

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,5 +1,17 @@
 "use client";
+import { pb } from "@/lib/pocketbase";
 import { ReactElement, useEffect, useState } from "react";
+
 export default function Recipe(): ReactElement {
-  return <div>레시피 페이지</div>;
+  const [recipeList, setRecipeList] = useState<any | null>();
+  useEffect(() => {
+    async function getCookData() {
+      const data = await pb.collection("cook").getFullList();
+      console.log(data);
+      setRecipeList(data);
+    }
+    getCookData();
+  }, []);
+
+  return <div>{recipeList}</div>;
 }
